1c1ad8cd3b25574f1fbfb520d543612d03b6244e
czgaotian <811460602@qq.com>
2025-03-19 14:53:18 +0100

    temp remove i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh-CN'],
+    locales: ['en'],
   },
   localePath: path.resolve('./public/locales'),
 };
