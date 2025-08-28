11e14320a4ce109b151656351a4414a2a2aa5193
czgaotian <811460602@qq.com>
2025-08-28 18:21:43 +0200

    fix: little style fix

--- themes/nobelium/components/BlogPost.tsx
+++ themes/nobelium/components/BlogPost.tsx
@@ -15,7 +15,7 @@ const BlogPost: FC<BlogPostProps> = ({ post, isShowSummary = true }) => {
         key={post.id}
         className="mb-4 rounded px-3 py-2 hover:bg-gray-200/40 md:mb-6 dark:hover:bg-gray-800/40"
       >
-        <header className="mb-2 flex flex-col justify-between md:flex-row md:items-baseline">
+        <header className="flex flex-col justify-between md:flex-row md:items-baseline">
           <div className="mr-4 cursor-pointer text-lg font-medium text-gray-900 md:text-xl dark:text-gray-200">
             {post.title}
           </div>
@@ -24,7 +24,7 @@ const BlogPost: FC<BlogPostProps> = ({ post, isShowSummary = true }) => {
           </time>
         </header>
         {isShowSummary && post.summary && (
-          <div className="hidden leading-8 text-gray-700 md:block dark:text-gray-300">
+          <div className="mt-2 hidden leading-8 text-gray-700 md:block dark:text-gray-300">
             {post.summary}
           </div>
         )}
