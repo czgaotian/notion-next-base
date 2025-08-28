164d4882cbccbafb4590a5899d48829439919a4d
czgaotian <811460602@qq.com>
2025-08-28 14:42:25 +0200

    feat: update title style in nobelium theme

--- components/NotionIcon.tsx
+++ components/NotionIcon.tsx
@@ -1,20 +1,32 @@
 import LazyImage from './LazyImage';
+import classnames from '@/utils/classnames';
 
 /**
  * notion的图标icon
  * 可能是emoji 可能是 svg 也可能是 图片
  * @returns
  */
-const NotionIcon = ({ icon }: { icon: string }) => {
+const NotionIcon = ({
+  icon,
+  className = '',
+}: {
+  icon: string;
+  className?: string;
+}) => {
   if (!icon) {
     return <></>;
   }
 
   if (icon.startsWith('http') || icon.startsWith('data:')) {
-    return <LazyImage src={icon} className="my-auto mr-1 inline h-8 w-8" />;
+    return (
+      <LazyImage
+        src={icon}
+        className={classnames('my-auto mr-1 inline h-8 w-8', className)}
+      />
+    );
   }
 
-  return <span className="mr-1">{icon}</span>;
+  return <span className={classnames('mr-1', className)}>{icon}</span>;
 };
 
 export default NotionIcon;
--- themes/nobelium/components/ArticleInfo.tsx
+++ themes/nobelium/components/ArticleInfo.tsx
@@ -29,33 +29,32 @@ export const ArticleInfo = () => {
 
   return (
     <section className="mb-4 mt-2 flex flex-col font-light">
+      {post?.pageIcon && (
+        <NotionIcon className="mb-1 text-3xl" icon={post?.pageIcon} />
+      )}
       <div className="text-3xl font-bold text-black dark:text-white">
-        {post?.pageIcon && <NotionIcon icon={post?.pageIcon} />}
         {post?.title}
       </div>
-
       {post?.type !== 'Page' && (
         <>
           <div className="mt-7 flex items-center text-gray-500 dark:text-gray-500">
-            <Link
-              href={GITHUB_URL || '#'}
-              className="mr-1 h-6 w-6 overflow-hidden"
-            >
+            <Link href={GITHUB_URL || '#'} className="flex items-center">
               <Image
-                className="h-6 w-6 rounded-full"
+                className="mr-1 h-6 w-6 rounded-full"
                 alt={AUTHOR}
                 width={24}
                 height={24}
                 src={`https://gravatar.com/avatar/${emailHash}`}
               />
+              <div className="mr-4 md:block">{AUTHOR}</div>
             </Link>
-            <div className="mr-4 md:block">{AUTHOR}</div>
             <div className="mr-4 md:ml-0">
               {dayjs(post?.date).format('YYYY-MM-DD')}
             </div>
           </div>
-          <div className="mt-4">
-            {post?.tags && (
+
+          {post?.tags && post.tags.length > 0 && (
+            <div className="mt-4">
               <div className="article-tags mr-2 flex max-w-full flex-nowrap overflow-x-auto">
                 {post?.tags.map((tag) => (
                   <TagItem
@@ -66,8 +65,8 @@ export const ArticleInfo = () => {
                   />
                 ))}
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </>
       )}
     </section>
